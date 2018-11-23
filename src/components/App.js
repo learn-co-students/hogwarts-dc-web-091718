24da37523d6cba24d9c5e38e1e870a602aded436
nachoespino53 <nachoespino53@gmail.com>
2018-11-23 13:31:18 -0500

    What I have so far

--- src/components/App.js
+++ src/components/App.js
@@ -2,13 +2,24 @@ import React, { Component } from 'react';
 import '../App.css';
 import Nav from './Nav'
 import hogs from '../porkers_data';
+import Hog from './Hog'
 
 class App extends Component {
+
+  constructor () {
+    super();
+    this.state = {
+      allHogs: hogs
+    }
+  }
+
   render() {
     return (
       <div className="App">
           < Nav />
-
+          <ul className="ui grid container">
+            {this.state.allHogs.map(hogDetails => <Hog hog={hogDetails} />)}
+          </ul>
       </div>
     )
   }
